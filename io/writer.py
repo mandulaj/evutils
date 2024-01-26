90515bd0d38e339b97b2a7e58bbf4a961ce2f5d8
Jakub Mandula <jmandula@ethz.ch>
2024-01-26 17:08:40 +0100

    switch to a numba implementation of raw writer

--- io/writer.py
+++ io/writer.py
@@ -3,7 +3,50 @@ from datetime import datetime
 
 import numpy as np
 
+import numba as nb
 
+
+@nb.njit
+def get_raw_buffer(events: np.ndarray, last_lower12_ts: int, last_upper12_ts: int, last_y: int):
+    buffer = np.zeros(len(events) * 8, dtype=np.uint8)
+
+    length = 0
+
+    for ev in events:
+        upper12_ts = (int(ev['t']) & 0x0FFF000) >> 12
+        lower12_ts = int(ev['t']) & 0x00000FFF
+
+        if upper12_ts != last_upper12_ts:
+            # write new upper ts2000
+            last_upper12_ts = upper12_ts
+            value = 0x8000 | upper12_ts
+            buffer[length+1] = (value >> 8) & 0xFF
+            buffer[length] = value & 0xFF
+            length += 2
+        
+        if lower12_ts != last_lower12_ts:
+            # write new lower ts
+            last_lower12_ts = lower12_ts
+            value = 0x6000 | lower12_ts
+            buffer[length+1] = (value >> 8) & 0xFF
+            buffer[length] = value & 0xFF
+            length += 2
+                            
+        if last_y != ev['y']:
+            value = (0x0000 | int(ev['y']))
+            # write new y
+            buffer[length+1] = (value >> 8) & 0xFF
+            buffer[length] = value & 0xFF
+            length += 2
+            last_y = ev['y']
+        # write x and p
+        value = 0x2000 | int(ev['x']) | int(ev['p'] << 11)
+        buffer[length+1] = (value >> 8) & 0xFF
+        buffer[length] = value & 0xFF
+        length += 2
+
+
+    return buffer[:length], last_lower12_ts, last_upper12_ts, last_y
  
 
 
@@ -84,31 +127,14 @@ f"""% camera_integrator_name Prophesee
 
     def write(self, events: np.ndarray):
 
-
-        for ev in events:
-            upper12_ts = (int(ev['t']) & 0x0FFF000) >> 12
-            lower12_ts = int(ev['t']) & 0x00000FFF
-
-            if upper12_ts != self.last_upper12_ts:
-                # write new upper ts2000
-                self.last_upper12_ts = upper12_ts
-                value = 0x8000 | upper12_ts
-                self.fd.write(value.to_bytes(2, byteorder='little'))
-            
-            if lower12_ts != self.last_lower12_ts:
-                # write new lower ts
-                self.last_lower12_ts = lower12_ts
-                value = 0x6000 | lower12_ts
-                self.fd.write(value.to_bytes(2, byteorder='little'))
-                                
-            if self.last_y != ev['y']:
-                value = (0x0000 | int(ev['y']))
-                # write new y
-                self.fd.write(value.to_bytes(2, byteorder='little'))
-                self.last_y = ev['y']
-            # write x and p
-            value = 0x2000 | int(ev['x']) | int(ev['p'] << 11)
-            self.fd.write(value.to_bytes(2, byteorder='little'))
+        buffer, self.last_lower12_ts, self.last_upper12_ts, self.last_y = get_raw_buffer(
+            events, 
+            self.last_lower12_ts, 
+            self.last_upper12_ts, 
+            self.last_y)
+        
+        buffer.tofile(self.fd)
+        
 
 
 class EventWriter_CSV(EventWriter):
