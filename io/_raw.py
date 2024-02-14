9b4a33f0e289fc7b2e30afdc92b1eff761c96f5a
Jakub Mandula <jakub@mandula.cz>
2024-02-14 18:01:13 +0100

    add types and clean up evt3 writer

--- io/_raw.py
+++ io/_raw.py
@@ -10,46 +10,59 @@ from ._reader import EventReader
 
 
 @nb.njit
-def get_raw_buffer(events: np.ndarray, last_lower12_ts: int, last_upper12_ts: int, last_y: int):
+def get_raw_evt3_buffer(events: np.ndarray, last_lower12_ts: int, last_upper12_ts: int, last_y: int, master = True):
+    # Pre-allocate large buffer
     buffer = np.zeros(len(events) * 8, dtype=np.uint8)
 
-    length = 0
+    # Prepare the master/slave bit
+    if master:
+        master_slave = 0x000
+    else:
+        master_slave = 0x800
+
+    # Current position of the buffer
+    i = 0
 
     for ev in events:
         upper12_ts = (int(ev['t']) & 0x0FFF000) >> 12
         lower12_ts = int(ev['t']) & 0x00000FFF
 
+        # EVT_TIME_HIGH - Updates the higher 12-bit portion of the 24-bit time base ‘1000’
         if upper12_ts != last_upper12_ts:
-            # write new upper ts2000
             last_upper12_ts = upper12_ts
             value = 0x8000 | (upper12_ts & 0xFFF)
-            buffer[length+1] = (value >> 8) & 0xFF
-            buffer[length] = value & 0xFF
-            length += 2
+            
+            buffer[i] = value & 0xFF
+            buffer[i+1] = (value >> 8) & 0xFF
+            i += 2
         
+        # EVT_TIME_LOW - Updates the lower 12-bit portion of the 24-bit time base ‘0110’
         if lower12_ts != last_lower12_ts:
-            # write new lower ts
             last_lower12_ts = lower12_ts 
             value = 0x6000 | (lower12_ts & 0xFFF)
-            buffer[length+1] = (value >> 8) & 0xFF
-            buffer[length] = value & 0xFF
-            length += 2
-                            
+            
+            buffer[i] = value & 0xFF
+            buffer[i+1] = (value >> 8) & 0xFF
+            i += 2
+
+        # EVT_ADDR_Y - Y coordinate, and system type (master/slave camera) ‘0000’
         if last_y != ev['y']:
-            value = (0x0000 | (int(ev['y']) & 0xFFF))
-            # write new y
-            buffer[length+1] = (value >> 8) & 0xFF
-            buffer[length] = value & 0xFF
-            length += 2
             last_y = ev['y']
-        # write x and p
-        value = 0x2000 | (int(ev['x']) & 0xFFF) | ((int(ev['p']) & 0x01) << 11)
-        buffer[length+1] = (value >> 8) & 0xFF
-        buffer[length] = value & 0xFF
-        length += 2
+            value = (0x0000 | master_slave | (int(ev['y']) & 0x7FF))
+            
+            buffer[i] = value & 0xFF
+            buffer[i+1] = (value >> 8) & 0xFF
+            i += 2
+        
+        # EVT_ADDR_X - Single valid event, X coordinate and polarity ‘0010’
+        value = 0x2000 | (int(ev['x']) & 0x7FF) | ((int(ev['p']) & 0x01) << 11)
+        
+        buffer[i] = value & 0xFF
+        buffer[i+1] = (value >> 8) & 0xFF
+        i += 2
 
 
-    return buffer[:length], last_lower12_ts, last_upper12_ts, last_y
+    return buffer[:i], last_lower12_ts, last_upper12_ts, last_y
 
 
 
@@ -63,8 +76,14 @@ class EventReader_RAW(EventReader):
 
  
 class EventWriter_RAW(EventWriter):
-    def __init__(self, file, width=1280, height=720, dt=None, serial="00000000"):
+    FORMATS = {"EVT3": "evt 3.0", "EVT2.1": "evt 2.1", "EVT2": "evt 2.1"}
+    def __init__(self, file, width=1280, height=720, dt=None, serial="00000000", format="EVT3"):
         super().__init__(file, width, height, dt)
+
+        if format not in self.FORMATS.keys():
+            raise ValueError(f"Unsupported format {format}. Supported formats are {list(EventWriter_RAW.FORMATS.keys())}")
+        self.format = format
+        self.system_id = 49
 
         self.was_initialized = False
 
@@ -76,9 +95,6 @@ class EventWriter_RAW(EventWriter):
        
         self.formatted_datetime = self.dt.strftime("%Y-%m-%d %H:%M:%S")
 
-        self.init()
-
-
     def init(self):
         if self.was_initialized:
             return
@@ -88,8 +104,8 @@ class EventWriter_RAW(EventWriter):
         self.fd.write(
 f"""% camera_integrator_name Prophesee
 % date {self.formatted_datetime}
-% evt 3.0
-% format EVT3;height={self.height};width={self.width}
+% {self.FORMATS[self.format]}
+% format {self.format};height={self.height};width={self.width}
 % generation 4.2
 % geometry {self.width}x{self.height}
 % integrator_name Prophesee
@@ -97,20 +113,27 @@ f"""% camera_integrator_name Prophesee
 % plugin_name hal_plugin_prophesee
 % sensor_generation 4.2
 % serial_number {self.serial_number}
-% system_ID 49
+% system_ID {self.system_id}
 % end
 """.encode('utf-8'))
         self.was_initialized = True
         
     def close(self):
-        self.fd.close()
+        if self.was_initialized:
+            self.fd.close()
 
     def write(self, events: np.ndarray):
 
-        buffer, self.last_lower12_ts, self.last_upper12_ts, self.last_y = get_raw_buffer(
-            events, 
-            self.last_lower12_ts, 
-            self.last_upper12_ts, 
-            self.last_y)
-        
+        if not self.was_initialized:
+            self.init()
+
+        if self.format == "EVT3":
+            buffer, self.last_lower12_ts, self.last_upper12_ts, self.last_y = get_raw_evt3_buffer(
+                events, 
+                self.last_lower12_ts, 
+                self.last_upper12_ts, 
+                self.last_y)
+        else:
+            raise NotImplementedError(f"Unsupported format {self.format}. Supported formats are {list(EventWriter_RAW.FORMATS.keys())}")
+
         buffer.tofile(self.fd)
---
+++ types/__init__.py
@@ -0,0 +1,7 @@
+
+
+
+import numpy as np
+
+
+Events = np.dtype([('t', np.uint64), ('x', np.uint16), ('y', np.uint16), ('p', np.uint8)])
