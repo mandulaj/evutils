70235ba7dc48e0e063fb95e80489bc1f18d3ffac
Jakub Mandula <jakub@mandula.cz>
2024-02-27 17:34:41 +0100

    add try/catch to import the src/io reader/writer

--- src/evutils/io/writer.py
+++ src/evutils/io/writer.py
@@ -4,14 +4,54 @@ from ._writer import EventWriter
 import numpy as np
 
  
+try:
+    from ._bin import EventWriter_Bin
+except ImportError:
+    class EventWriter_Bin(EventWriter):
+        def __init__(self, file, width=1280, height=720):
+            raise ImportError("EventWriter_Bin not available.")
+        
+try:
+    from ._csv import EventWriter_Csv
+except ImportError:
+    class EventWriter_Csv(EventWriter):
+        def __init__(self, file, width=1280, height=720):
+            raise ImportError("EventWriter_Csv not available.")
 
-from ._bin import EventWriter_Bin
-from ._csv import EventWriter_Csv
-from ._dat import EventWriter_Dat
-from ._hdf5 import EventWriter_HDF5
-from ._npz import EventWriter_Npz
-from ._raw import EventWriter_RAW
-from ._txt import EventWriter_Txt
+try:
+    from ._dat import EventWriter_Dat
+except ImportError:
+    class EventWriter_Dat(EventWriter):
+        def __init__(self, file, width=1280, height=720):
+            raise ImportError("EventWriter_Dat not available.")
+
+try:
+    from ._hdf5 import EventWriter_HDF5
+except ImportError:
+    class EventWriter_HDF5(EventWriter):
+        def __init__(self, file, width=1280, height=720):
+            raise ImportError("EventWriter_HDF5 not available.")
+
+try:
+    from ._npz import EventWriter_Npz
+except ImportError:
+    class EventWriter_Npz(EventWriter):
+        def __init__(self, file, width=1280, height=720):
+            raise ImportError("EventWriter_Npz not available.")
+
+try:
+    from ._raw import EventWriter_RAW
+except ImportError:
+    class EventWriter_RAW(EventWriter):
+        def __init__(self, file, width=1280, height=720):
+            raise ImportError("EventWriter_RAW not available.")
+
+try:
+    from ._txt import EventWriter_Txt
+except ImportError:
+    class EventWriter_Txt(EventWriter):
+        def __init__(self, file, width=1280, height=720):
+            raise ImportError("EventWriter_Txt not available.")
 
 
 
