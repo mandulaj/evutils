509231cae863e9b40ec0c6f0920207156e106797
Jakub Mandula <jakub@mandula.cz>
2024-02-28 14:45:50 +0100

    switch to pandas writer for speed

--- src/evutils/io/_csv.py
+++ src/evutils/io/_csv.py
@@ -3,6 +3,7 @@ from ._writer import EventWriter
 from ._reader import EventReader
 
 import numpy as np
+import pandas as pd
 
 
 class EventReader_Csv(EventReader):
@@ -12,16 +13,33 @@ class EventReader_Csv(EventReader):
 
 
 class EventWriter_Csv(EventWriter):
-    def __init__(self, file, width=1280, height=720):
+    def __init__(self, file, width=1280, height=720, order=['t', 'x', 'y', 'p'], header=True):
         super().__init__(file, width, height)
+
+        if len(order) != 4:
+            raise ValueError("Order must be a list of 4 strings")
+        if "t" not in order or "x" not in order or "y" not in order or "p" not in order:
+            raise ValueError("Order must contain 't', 'x', 'y' and 'p'")
+        
+        self.order = order
+        self.header = header
 
     def init(self):
         self.fd = open(self.file, "w")
-        self.fd.write("t, x, y, p\n")
+
+        if self.header:
+            self.fd.write(",".join(self.order) + "\n")
+
+        self.is_initialized = True
+
 
     def write(self, events: np.ndarray):
-        for ev in events:
-            self.fd.write(f"{ev['t']}, {ev['x']}, {ev['y']}, {ev['p']}\n")
+        if not self.is_initialized:
+            self.init()
+            
+
+        df = pd.DataFrame(events)
+        df.to_csv(self.fd, mode='a', header=False, index=False, columns=self.order)
 
     def close(self):
         self.fd.close()
