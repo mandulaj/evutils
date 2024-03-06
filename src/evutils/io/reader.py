474ba76486404d169f9fcd46353cff9b352121d9
Jakub Mandula <jakub@mandula.cz>
2024-03-06 15:09:46 +0100

    make sure the error is properly propagated

--- src/evutils/io/reader.py
+++ src/evutils/io/reader.py
@@ -6,52 +6,59 @@ import numpy as np
 
 try:
     from ._bin import EventReader_Bin
-except ImportError:
+except ImportError as e:
+    error = str(e)
     class EventReader_Bin(EventReader):
         def __init__(self, file):
-            raise ImportError("EventReader_Bin not available.")
+            raise ImportError("EventReader_Bin not available: " +  error)
 
 try:
     from ._csv import EventReader_Csv
-except ImportError:
+except ImportError as e:
+    error = str(e)
     class EventReader_Csv(EventReader):
         def __init__(self, file):
-            raise ImportError("EventReader_Csv not available.")
+            raise ImportError("EventReader_Csv not available: " +  error)
         
 try:
     from ._dat import EventReader_Dat
-except ImportError:
+except ImportError as e:
+    error = str(e)
     class EventReader_Dat(EventReader):
         def __init__(self, file):
-            raise ImportError("EventReader_Dat not available.")
+            raise ImportError("EventReader_Dat not available: " +  error)
         
 try:
     from ._hdf5 import EventReader_HDF5
-except ImportError:
+except ImportError as e:
+    error = str(e)
     class EventReader_HDF5(EventReader):
         def __init__(self, file):
-            raise ImportError("EventReader_HDF5 not available.")
+            raise ImportError("EventReader_HDF5 not available: " +  error)
         
 try:
     from ._npz import EventReader_Npz
-except ImportError:
+except ImportError as e:
+    error = str(e)
     class EventReader_Npz(EventReader):
         def __init__(self, file):
-            raise ImportError("EventReader_Npz not available.")
+            raise ImportError("EventReader_Npz not available: " +  error)
 
 try:
     from ._raw import EventReader_RAW
-except ImportError:
+except ImportError as e:
+    error = str(e)
     class EventReader_RAW(EventReader):
         def __init__(self, file):
-            raise ImportError("EventReader_RAW not available.")
+            raise ImportError("EventReader_RAW not available: " +  error)
 
 try:
     from ._txt import EventReader_Txt
-except ImportError:
+except ImportError as e:
+    error = str(e)
     class EventReader_Txt(EventReader):
         def __init__(self, file):
-            raise ImportError("EventReader_Txt not available.")
+            raise ImportError("EventReader_Txt not available: " +  error)
         
 
 
--- src/evutils/io/writer.py
+++ src/evutils/io/writer.py
@@ -6,52 +6,59 @@ import numpy as np
  
 try:
     from ._bin import EventWriter_Bin
-except ImportError:
+except ImportError as e:
+    error = str(e)
     class EventWriter_Bin(EventWriter):
         def __init__(self, file, width=1280, height=720):
-            raise ImportError("EventWriter_Bin not available.")
+            raise ImportError("EventWriter_Bin not available: " +  error)
         
 try:
     from ._csv import EventWriter_Csv
-except ImportError:
+except ImportError as e:
+    error = str(e)
     class EventWriter_Csv(EventWriter):
         def __init__(self, file, width=1280, height=720):
-            raise ImportError("EventWriter_Csv not available.")
+            raise ImportError("EventWriter_Csv not available: " +  error)
 
 try:
     from ._dat import EventWriter_Dat
-except ImportError:
+except ImportError as e:
+    error = str(e)
     class EventWriter_Dat(EventWriter):
         def __init__(self, file, width=1280, height=720):
-            raise ImportError("EventWriter_Dat not available.")
+            raise ImportError("EventWriter_Dat not available: " +  error)
 
 try:
-    from ._hdf5 import EventWriter_HDF5
-except ImportError:
+    from  ._hdf5 import EventWriter_HDF5
+except ImportError as e:
+    error = str(e)
     class EventWriter_HDF5(EventWriter):
         def __init__(self, file, width=1280, height=720):
-            raise ImportError("EventWriter_HDF5 not available.")
+            raise ImportError("EventWriter_HDF5 not available: " +  error)
 
 try:
     from ._npz import EventWriter_Npz
-except ImportError:
+except ImportError as e:
+    error = str(e)
     class EventWriter_Npz(EventWriter):
         def __init__(self, file, width=1280, height=720):
-            raise ImportError("EventWriter_Npz not available.")
+            raise ImportError("EventWriter_Npz not available: " +  error)
 
 try:
     from ._raw import EventWriter_RAW
-except ImportError:
+except ImportError as e:
+    error = str(e)
     class EventWriter_RAW(EventWriter):
         def __init__(self, file, width=1280, height=720):
-            raise ImportError("EventWriter_RAW not available.")
+            raise ImportError("EventWriter_RAW not available: " +  error)
 
 try:
     from ._txt import EventWriter_Txt
-except ImportError:
+except ImportError as e:
+    error = str(e)
     class EventWriter_Txt(EventWriter):
         def __init__(self, file, width=1280, height=720):
-            raise ImportError("EventWriter_Txt not available.")
+            raise ImportError("EventWriter_Txt not available: " +  error)
 
 
 
