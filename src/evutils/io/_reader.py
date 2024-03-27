df137f5e367a40bcd57bedf024f1a316c54e248e
Jakub Mandula <jakub@mandula.cz>
2024-03-27 18:32:47 +0100

    rename types and move common code to base function

--- src/evutils/io/_reader.py
+++ src/evutils/io/_reader.py
@@ -1,6 +1,8 @@
 
 import numpy as np
 
+from pathlib import Path
+
 import os
 
 class EventReader():
@@ -30,54 +32,121 @@ class EventReader():
 
     '''
     READING_MODES = ["delta_t", "n_events", "mixed", "all", "auto"]
-    def __init__(self, file, delta_t=None, n_events=None, max_events=10000000, mode="auto"):
+    DEFAULT_N_EVENTS = 10_000
+    DEFAULT_DELTA_T = 10_000
+    def __init__(self, file, delta_t=None, n_events=None,  mode="auto", start_ts=0, max_time=1_000_000_000_000, max_events=10_000_000, width=None, height=None):
+        print("EventReader constructor")
 
-
-        self.file = file
+        self.file = Path(file)
         self.eof = False
         self.fd = None 
-        self.width = None
-        self.height = None
+        self.width = width
+        self.height = height
+        self.start_ts = start_ts
 
+        # Maximum number of events to read and maximum time to read in a chunk
+        self.max_events = max_events
+        self.max_time = max_time
+
+
+        # Validate the parameters
         if not mode in EventReader.READING_MODES:
             raise ValueError(f"Mode {mode} not supported. Supported modes are: {EventReader.READING_MODES}")
+        
+        if not self.file.exists() or not self.file.is_file():
+            raise FileNotFoundError(f"File {self.file} does not exist")
+        
+        if not isinstance(self.max_events, int):
+            raise TypeError("max_events must be an integer")
+        
+        if not isinstance(self.max_time, int):
+            raise TypeError("max_time must be an integer")
+        
+        if not isinstance(self.start_ts, int):
+            raise TypeError("start_ts must be an integer")
+        
+
+
         self.mode = mode
 
         # if mode is auto, we will try to infer the mode from the parameters
         if self.mode == "auto":
+            # If both delta_t and n_events are specified, we will use mixed mode
             if delta_t is not None and n_events is not None:
                 self.mode = "mixed"
+
+            # If only one of the parameters is specified, we will use that mode, the other will be set to the maximum
             elif delta_t is not None:
                 self.mode = "delta_t"
-                n_events = -1
+                n_events = self.max_events
             elif n_events is not None:
                 self.mode = "n_events"
-                delta_t = -1
+                delta_t = self.max_time
             else:
-                delta_t = -1
-                n_events = -1
+                # If none of the parameters are specified, we will use the default Values
                 self.mode = "mixed"
+                delta_t = self.DEFAULT_DELTA_T
+                n_events = self.DEFAULT_N_EVENTS
+
+        # If the mode is not auto, we will check if the parameters are specified
         elif self.mode == "delta_t":
             if delta_t is None:
                 raise ValueError("delta_t must be specified")
+            n_events = self.max_events
         elif self.mode == "n_events":
             if n_events is None:
                 raise ValueError("n_events must be specified")
+            delta_t = self.max_time
+        elif self.mode == "mixed":
+            if delta_t is None:
+                delta_t = self.DEFAULT_DELTA_T  
+            if n_events is None:
+                n_events = self.DEFAULT_N_EVENTS
 
+        elif self.mode == "all":
+            delta_t = self.max_time
+            n_events = self.max_events
 
-        self.delta_t = delta_t if delta_t > 0 else 10000
-        self.n_events = n_events if n_events > 0 else max_events
-        self.max_events = max_events
+        
+        # Validate the parameters
+        if delta_t is None:
+            delta_t = self.DEFAULT_DELTA_T
+        if n_events is None:
+            n_events = self.DEFAULT_N_EVENTS
+
+        if not isinstance(delta_t, int):
+            raise TypeError("delta_t must be an integer")
+
+        if not isinstance(n_events, int):
+            raise TypeError("n_events must be an integer")
+
+        
+        if delta_t <= 0:
+            raise ValueError("delta_t must be positive")
+        
+        if n_events <= 0:
+            raise ValueError("n_events must be positive")
+        
+
+        self.delta_t = delta_t
+        self.n_events = n_events
 
         self.is_initialized = False
 
         self.n_read_events = 0
+        self.buffer_len = 0
+        self.buffer = None
 
     
     def init(self):
         '''
         Initialize the reader, can be used explicitly or implicitly by the read method.
         '''
+        raise NotImplementedError
+    
+
+    def _read(self, delta_t:int, n_events:int) -> np.ndarray:
+        '''Reads the next n_events or delta_t events from the file, which ever comes first and returns them as a numpy array'''
         raise NotImplementedError
     
     def read(self, delta_t:int=None, n_events:int=None) -> np.ndarray:
@@ -102,8 +171,29 @@ class EventReader():
             If the end of the file is reached
 
         '''
-        raise NotImplementedError
-    
+        # If not initialized, initialize
+        if not self.is_initialized:
+            self.init()
+
+        # Override the parameters if they are specified
+        if delta_t is None:
+            delta_t = self.delta_t
+        if n_events is None:
+            n_events = self.n_events
+
+        # if self.mode == "delta_t":
+        #     n_events = self.max_events
+        #     self._read(delta_t, n_events)
+        # elif self.mode == "n_events":
+        #     delta_t = self.max_time
+        # elif self.mode == "mixed":
+        #     pass
+        # elif self.mode == "all":
+        #     pass
+
+        return self._read(delta_t, n_events)
+
+
     def __enter__(self):
         return self
     
@@ -124,7 +214,8 @@ class EventReader():
         '''
         Close the file and release the resources
         '''
-        raise NotImplementedError
+        if self.is_initialized and self.fd is not None:
+            self.fd.close()
 
     def __exit__(self, exc_type, exc_value, traceback):
         self.close()
@@ -149,10 +240,13 @@ class EventReader():
             A numpy array with the events
         
         '''
-        
+        if not self.is_initialized:
+            self.init()
         while not self.is_eof():
-            yield self.read()
+            yield self._read(self.delta_t, self.n_events)
 
+    def shape(self) -> tuple[int, int]:
+        return self.width, self.height
     
     def file_size(self) -> int:
         '''
