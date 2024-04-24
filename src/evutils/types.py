5a33ac9140eed74e93751440be4081d1c3ba8f39
Jakub Mandula <jakub@mandula.cz>
2024-04-24 13:04:14 +0200

    change EVType to int from uint

--- src/evutils/types.py
+++ src/evutils/types.py
@@ -4,7 +4,12 @@
 import numpy as np
 
 
-Events = np.dtype([('t', np.uint64), ('x', np.uint16), ('y', np.uint16), ('p', np.uint8)])
+Events = np.dtype([('t', np.int64), ('x', np.uint16), ('y', np.uint16), ('p', np.uint8)])
 
-Triggers = np.dtype([('t', np.uint64), ('p', np.uint8), ('id', np.uint8)])
+Triggers = np.dtype([('t', np.int64), ('p', np.uint8), ('id', np.uint8)])
+
+
+def is_monotonically_increasing(events: np.ndarray) -> bool:
+    '''Checks if the event ts is monotonically increasing'''
+    return np.all(np.diff(events['t']) >= 0)
 
--- tests/test_types.py
+++ tests/test_types.py
@@ -6,3 +6,12 @@ from evutils.types import Events
 def test_event_size():
     a = np.zeros(1, dtype=Events)
     assert a.itemsize == 13
+
+
+def test_event_dtype():
+    a = np.zeros(1, dtype=Events)
+    a['t'] = 1
+    a['x'] = 2
+    a['y'] = 3
+    a['p'] = 4
+    
