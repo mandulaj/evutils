e97ed379b617f2672a8abcb42664faf09bf1198b
Jakub Mandula <jmandula@ethz.ch>
2024-07-30 17:48:02 +0200

    add documentation for csv

--- src/evutils/io/_csv.py
+++ src/evutils/io/_csv.py
@@ -7,6 +7,34 @@ import pandas as pd
 
 
 class EventReader_Csv(EventReader):
+    '''
+    A reader for CSV files with events.
+
+    Parameters
+    ----------
+    file : str
+        Path to the data file
+    delta_t : int, optional
+        Time interval between events in microseconds, by default None
+    n_events : int, optional
+        Number of events to read in a chunk, by default None
+    max_events : int, optional
+        Maximum number of events to read at once, by default 10,000
+    mode : {"auto", "delta_t", "n_events", "mixed", "all" }, optional
+        Mode of operation, by default "auto"
+    buffer_size : int, optional
+        Number of events to read at once, by default 1,000,000
+    order : list, optional
+        Order of the columns in the CSV file, by default ['t', 'x', 'y', 'p']
+    header : bool, optional
+        If True, the first line is considered a header, by default True
+
+    Raises
+    ------
+    ValueError
+        If the order is not a list of 4 strings or if the order does not contain 't', 'x', 'y' and 'p'
+    
+    '''
     def __init__(self, file,  delta_t=None, n_events=None, max_events=10_000, mode="auto", buffer_size=1_000_000, order=['t', 'x', 'y', 'p'], header=True):
         super().__init__(file, delta_t, n_events, max_events, mode)
 
@@ -60,6 +88,33 @@ class EventReader_Csv(EventReader):
 
 
 class EventWriter_Csv(EventWriter):
+    '''
+    A writer for CSV files with events.
+
+    Parameters
+    ----------
+    file : str
+        Path to the data file
+    width : int, optional
+        Width of the frame, by default 1280 (not relevant for this formats)
+    height : int, optional
+        Height of the frame, by default 720 (not relevant for this formats)
+    sep : str, optional 
+        Separator for the CSV file, by default ","
+    order : list, optional
+        Order of the columns in the CSV file, by default ['t', 'x', 'y', 'p']
+    header : bool, optional
+        If True, a header is written on the first line, by default True
+
+    Raises
+    ------
+    ValueError
+        If the order is not a list of 4 strings or if the order does not contain 't', 'x', 'y' and 'p'
+
+    '''
+    
+    
+    
     def __init__(self, file, width=1280, height=720, sep=",", order=['t', 'x', 'y', 'p'], header=True):
         super().__init__(file, width, height)
 
