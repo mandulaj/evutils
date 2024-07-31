d8a410b00b1ecb106a76e6d119f7f851449aa70d
Jakub Mandula <jmandula@ethz.ch>
2024-07-31 14:03:31 +0200

    change to signed int ts in Events type

--- src/evutils/types.py
+++ src/evutils/types.py
@@ -12,21 +12,21 @@ __all__ = ['Events', 'Triggers']
 #:
 #: Fields:
 #:
-#: - `t` (np.uint64): Timestamp of the event (us).
+#: - `t` (np.int64): Timestamp of the event (us).
 #: - `x` (np.uint16): X-coordinate.
 #: - `y` (np.uint16): Y-coordinate.
 #: - `p` (np.uint8): Polarity (0: off, 1: on).
-Events = np.dtype([('t', np.uint64), ('x', np.uint16), ('y', np.uint16), ('p', np.uint8)])
+Events = np.dtype([('t', np.int64), ('x', np.uint16), ('y', np.uint16), ('p', np.uint8)])
 
 
 #: A structured numpy dtype for trigger data.
 #:
 #: Fields:
 #:
-#: - `t` (np.uint64): Timestamp of the event (us).
+#: - `t` (np.int64): Timestamp of the event (us).
 #: - `p` (np.uint8):  Polarity (0: off, 1: on).
 #: - `id` (np.uint8): Identifier.
-Triggers = np.dtype([('t', np.uint64), ('p', np.uint8), ('id', np.uint8)])
+Triggers = np.dtype([('t', np.int64), ('p', np.uint8), ('id', np.uint8)])
 
 def is_monotonically_increasing(events: np.ndarray) -> bool:
     '''Checks if the event ts is monotonically increasing'''
