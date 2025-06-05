b1f3b941920b82dae1dd57e6f597ecaeb5cfd862
Jakub Mandula <jakub.mandula@pbl.ee.ethz.ch>
2025-06-05 23:03:14 +0200

    add more cool 3d plots

--- src/evutils/vis/plot3d.py
+++ src/evutils/vis/plot3d.py
@@ -1,6 +1,6 @@
 import numba 
 import numpy as np
-
+import cv2
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
@@ -67,7 +67,116 @@ def plot_3d(events: np.ndarray,
     ax.set_title('Event Stream 3D Plot')
     ax.set_xlim([ts.min(), ts.max()])
     ax.set_ylim([0, width])
-    ax.set_zlim([0, height])
+    ax.set_zlim([height, 0])
     ax.set_box_aspect([max(width,height), width, height])  # Aspect ratio
 
+    return fig, ax
+
+
+def plot_3d_histogram(histogram: np.ndarray,
+                      down_sample: int = 4,
+                      fig: Optional[plt.Figure] = None,
+                      ax: Optional[Axes3D] = None) -> tuple:
+    """Plot a 3D histogram of events.
+    Parameters
+    ----------
+    histogram : np.ndarray
+        3D histogram array with shape (depth, height, width).
+    down_sample : int, optional
+        Downsampling factor for the histogram, by default 4.
+    fig : Optional[plt.Figure], optional
+        Matplotlib figure to plot on, by default None (a new figure will be created).
+    ax : Optional[Axes3D], optional
+        Matplotlib 3D axis to plot on, by default None (a new axis will be created).
+    Returns
+    -------
+    tuple
+        A tuple containing the figure and axis objects.
+    """
+
+    # Create figure/axis if not provided
+    if ax is None:
+        fig = plt.figure(figsize=(10, 7)) if fig is None else fig
+        ax = fig.add_subplot(111, projection='3d')
+    # Create a meshgrid for the histogram
+
+    assert histogram.ndim == 4, "Histogram must be a 4D array."
+    n_bins, height, width, ch = histogram.shape
+
+    height_downsampled = height // down_sample
+    width_downsampled = width // down_sample
+    x, y = np.meshgrid(np.linspace(0, width - 1, width_downsampled),
+                       np.linspace(0, height - 1, height_downsampled))
+    
+
+    for bin in range(n_bins):
+        print(f"Plotting bin {bin+1}/{n_bins}")
+        image = histogram[bin]
+
+        # Downsample the image for better visualization
+        image = cv2.resize(image, (width_downsampled, height_downsampled), interpolation=cv2.INTER_LINEAR)
+
+        # Conver to RGBA
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGBA)
+
+        # Set the A channel to 0 where the pixel is black
+        image[:, :, 3] = np.where(np.all(image[:, :, :3] == 0, axis=-1), 0, 255)
+
+
+
+        z = np.full(x.shape, bin)
+
+        # Plot the surface
+        ax.plot_surface(z, x, y, facecolors=image/255, rstride=1, cstride=1, shade=False)
+
+        # Create a meshgrid for the histogram
+
+    ax.set_xlabel('bins')
+    ax.set_ylabel('x')
+    ax.set_zlabel('y')
+    ax.set_title('Voxel Histogram')
+    ax.set_xlim([0, n_bins])
+    ax.set_ylim([0, width])
+    ax.set_zlim([height, 0])
+    ax.set_box_aspect([max(width,height), width, height])  # Aspect ratio
+
+    return fig, ax
+
+
+
+
+def plot_3d_timesurface(events: np.ndarray, 
+            width: int =1280, 
+            height: int = 720,
+            tau: int = 10_000,
+            fig: Optional[plt.Figure] = None, 
+            ax: Optional[Axes3D] = None) -> tuple:   
+
+    ts = events['t'] # Convert timestamps to seconds
+
+    ts_ref = ts[-1]  # Reference timestamp for normalization
+    
+    p_sign = (events['p'].astype(np.float32) * 2 - 1)
+
+    colors = np.exp(-(ts_ref - ts) / tau) * p_sign  # Normalize polarity
+
+    ts = ts / 1_000  # Convert timestamps to milliseconds
+    # Create figure/axis if not provided
+    if ax is None:
+        fig = plt.figure(figsize=(10, 7)) if fig is None else fig
+        ax = fig.add_subplot(111, projection='3d')
+
+    # Plot as points
+    ax.scatter(ts, events['x'], events['y'], c=colors, s=1, alpha=0.2)
+
+    # Labels
+    ax.set_xlabel('time (ms)')
+    ax.set_ylabel('x')
+    ax.set_zlabel('y')
+    ax.set_title('Event Stream 3D Plot')
+    ax.set_xlim([ts.min(), ts.max()])
+    ax.set_ylim([0, width])
+    ax.set_zlim([height, 0])
+    ax.set_box_aspect([max(width,height), width, height])  # Aspect ratio
+    
     return fig, ax
