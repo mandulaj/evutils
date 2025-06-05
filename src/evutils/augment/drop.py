a15ae52349d2f9200ad599a4f260106df813a1a7
Jakub Mandula <jakub.mandula@pbl.ee.ethz.ch>
2025-06-05 13:23:54 +0200

    add drop augment

---
+++ src/evutils/augment/drop.py
@@ -0,0 +1,32 @@
+
+import numpy as np
+
+
+
+def drop_random_events(events: np.ndarray, drop_rate: float = 0.1):
+    """Drops a percentage of events randomly using slicing.
+    
+    Parameters
+    ----------
+    events : np.ndarray
+        Array of events to drop from.
+    drop_rate : float, optional
+        Percentage of events to drop, by default 0.1 (10%).
+    
+    Returns
+    -------
+    np.ndarray:w
+    
+        Array of events with the specified percentage dropped.
+    """
+    if drop_rate <= 0 or drop_rate >= 1:
+        raise ValueError("drop_rate must be between 0 and 1")
+    
+    n_events = len(events)
+    indices = np.arange(n_events)
+    np.random.shuffle(indices)
+    
+    drop_count = int(n_events * drop_rate)
+    keep_indices = indices[drop_count:]
+    
+    return events[keep_indices]
